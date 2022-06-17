d4835853afe401f9bb26e97512a920452b7cdcd9
Liz Ainslie <lizainslie16@gmail.com>
2022-06-16 22:38:48 -0700

    [Javadoc] Document util package

--- api/src/main/kotlin/dev/synapsetech/teampages/util/FailableResponse.kt
+++ api/src/main/kotlin/dev/synapsetech/teampages/util/FailableResponse.kt
@@ -1,5 +1,10 @@
 package dev.synapsetech.teampages.util
 
+/**
+ * A base interface for responses that are expected to fail in some way.
+ *
+ * @author Liz Ainslie
+ */
 interface FailableResponse {
     val success: Boolean
 }
--- api/src/main/kotlin/dev/synapsetech/teampages/util/ResponseError.kt
+++ api/src/main/kotlin/dev/synapsetech/teampages/util/ResponseError.kt
@@ -2,6 +2,14 @@ package dev.synapsetech.teampages.util
 
 import kotlinx.serialization.Serializable
 
+/**
+ * A base implementation of FailableResponse that indicates an error and offers
+ * an error message to the client.
+ *
+ * @see FailableResponse
+ *
+ * @author Liz Ainslie
+ */
 @Serializable
 data class ResponseError(
     val message: String,
