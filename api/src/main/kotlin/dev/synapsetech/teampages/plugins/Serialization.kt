5f2637872ee56971bc41eafb20cade915d2514d1
Liz Ainslie <lizainslie16@gmail.com>
2022-06-17 09:42:14 -0700

    Upgrade & migrate Ktor

--- api/src/main/kotlin/dev/synapsetech/teampages/plugins/Serialization.kt
+++ api/src/main/kotlin/dev/synapsetech/teampages/plugins/Serialization.kt
@@ -3,13 +3,9 @@ package dev.synapsetech.teampages.plugins
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
         json()
-    }
-    routing {
     }
 }
