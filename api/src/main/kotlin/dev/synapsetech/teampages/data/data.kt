e43d7b1a244b1c86167484b81c8b070ef24613a1
Liz Ainslie <lizainslie16@gmail.com>
2022-06-16 22:32:48 -0700

    [Javadoc] Document data symbols

--- api/src/main/kotlin/dev/synapsetech/teampages/data/data.kt
+++ api/src/main/kotlin/dev/synapsetech/teampages/data/data.kt
@@ -6,14 +6,31 @@ import dev.synapsetech.teampages.config.DatabaseConfigPart
 import org.litote.kmongo.KMongo
 import xyz.downgoon.snowflake.Snowflake
 
+/**
+ * Manages the connection to MongoDB.
+ *
+ * @author Liz Ainslie
+ */
 object Mongo {
     lateinit var mongoClient: MongoClient
     lateinit var database: MongoDatabase
 
+    /**
+     * Initialize and connect to MongoDB.
+     *
+     * @param dbConfig The database configuration to utilize.
+     *
+     * @author Liz Ainslie
+     */
     fun init(dbConfig: DatabaseConfigPart) {
         mongoClient = KMongo.createClient(dbConfig.mongoUri)
         database = mongoClient.getDatabase(dbConfig.mongoDatabase)
     }
 }
 
+/**
+ * A singleton instance of the Snowflake ID generator.
+ *
+ * @author Liz Ainslie
+ */
 val snowflake = Snowflake(0, 0)
