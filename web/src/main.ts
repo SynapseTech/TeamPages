714056a2b3e9ed34f5eb4329b8381d3bcd40a654
Liz Ainslie <lizainslie16@gmail.com>
2022-06-20 22:57:43 -0700

    Install sass and fontawesome

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
 
 import './assets/base.scss';
 
@@ -7,6 +11,8 @@ import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
+library.add(fas, fab);
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.use(createPinia());
 app.use(router);
