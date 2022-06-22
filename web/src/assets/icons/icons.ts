2e7122350c09112d21677ebca577c58cfd7d72ba
Liz Ainslie <lizainslie16@gmail.com>
2022-06-22 01:08:43 -0700

    Icons

---
+++ web/src/assets/icons/icons.ts
@@ -0,0 +1,9 @@
+import lock from './lock.svg?component';
+import user from './user.svg?component';
+import eye from './eye.svg?component';
+import eyeSlash from './eye-slash.svg?component';
+import envelope from './envelope.svg?component';
+import closeSquare from './close-square.svg?component';
+import danger from './danger.svg?component';
+
+export default { lock, user, eye, eyeSlash, envelope, closeSquare, danger };
---
+++ web/src/components/Icon.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+	import type { Component } from 'vue';
+	import { computed, defineProps, reactive } from 'vue';
+
+	import icons from '@/assets/icons/icons';
+
+	console.log(icons);
+
+	const emit = defineEmits(['tap']);
+
+	interface IconProps {
+		name: keyof typeof icons;
+		svgClass: string;
+	}
+
+	const props = defineProps<IconProps>();
+	const { svgClass } = reactive(props);
+	const icon = computed<Component>(() => icons[props.name]);
+
+	function tapped() {
+		emit('tap');
+	}
+</script>
+
+<template>
+	<div>
+		<KeepAlive>
+			<component :is="icon" @click="tapped" :class="svgClass" />
+		</KeepAlive>
+	</div>
+</template>
+
+<style scoped lang="scss"></style>
