36543f40782f3830ebda031990c35470032bdf94
Liz Ainslie <lizainslie16@gmail.com>
2022-06-22 01:09:38 -0700

    Icons

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,11 +1,17 @@
 import { fileURLToPath, URL } from 'url';
+import svgLoader from 'vite-svg-loader';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		svgLoader({
+			defaultImport: 'component',
+		}),
+	],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
