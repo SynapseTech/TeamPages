79935b32ac643e67ab373691700d44b700fd4378
Liz Ainslie <lizainslie16@gmail.com>
2022-06-29 09:39:53 -0700

    Add books page

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -14,6 +14,11 @@ const router = createRouter({
 			name: 'login',
 			component: () => import('../views/LoginView.vue'),
 		},
+		{
+			path: '/books',
+			name: 'books',
+			component: () => import('../views/BooksView.vue'),
+		},
 	],
 });
 
---
+++ web/src/views/BooksView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+	import MainToolbar from '@/components/MainToolbar.vue';
+</script>
+
+<template>
+	<main class="booksPage">
+		<MainToolbar />
+	</main>
+</template>
+
+<style lang="scss"></style>
